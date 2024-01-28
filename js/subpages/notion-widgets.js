5d6e9220071a1a8c3b94ffd534b51da2e3bd37c8
petracoding <petracoding@outlook.com>
2024-01-28 18:05:06 +0100

    Improvements and a cute font

--- js/subpages/notion-widgets.js
+++ js/subpages/notion-widgets.js
@@ -1,41 +1,41 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  if (document.querySelector(".notion-widgets")) {
-    const isLocal = false;
-    const urlBase = isLocal ? "http://localhost:52330/notion/" : "https://petracoding.github.io/notion/";
+const isLocal = false;
+const urlBase = isLocal ? "http://localhost:52330/notion/" : "https://petracoding.github.io/notion/";
 
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.querySelector(".notion-widgets")) {
     doText();
     const textOptionElements = document.querySelector("#text").querySelectorAll("input, select");
     [...textOptionElements].forEach((e) => e.addEventListener("input", doText));
+  }
 
-    function doText() {
-      const container = document.querySelector("#text");
-      let url = urlBase + "text.html";
-      const mode = container.querySelector('[name="text-mode"]:checked').value;
-      url += "?mode=" + mode;
-      const centered = container.querySelector("#text-centered").checked;
-      url += "&centered=" + (centered ? "1" : "0");
-      const size = container.querySelector("#text-size").value;
-      url += "&size=" + size;
-      const fullWidth = container.querySelector("#text-fullWidth").checked;
-      url += "&fullWidth=" + (fullWidth ? "1" : "0");
-      const color = container.querySelector("#text-color").value.replace("#", "");
-      url += "&color=" + color;
-      if (container.querySelector("#text-bg").checked) {
-        container.querySelector(".only-show-if-bg-color").style.visibility = "visible";
-        const background = container.querySelector("#text-background").value.replace("#", "");
-        url += "&background=" + background;
-        const corners = container.querySelector("#text-corners").value;
-        url += "&corners=" + corners;
-      } else {
-        container.querySelector(".only-show-if-bg-color").style.visibility = "hidden";
-      }
-      const font = container.querySelector("#text-font").value;
-      url += "&font=" + font;
-      const text = container.querySelector("#text-text").value;
-      url += "&text=" + encodeURIComponent(text);
-      container.querySelector(".output").value = url;
-      container.querySelector("iframe").setAttribute("src", url);
+  function doText() {
+    const container = document.querySelector("#text");
+    let url = urlBase + "text.html";
+    const mode = container.querySelector('[name="text-mode"]:checked').value;
+    url += "?mode=" + mode;
+    const centered = container.querySelector("#text-centered").checked;
+    url += "&centered=" + (centered ? "1" : "0");
+    const size = container.querySelector("#text-size").value;
+    url += "&size=" + size;
+    const fullWidth = container.querySelector("#text-fullWidth").checked;
+    url += "&fullWidth=" + (fullWidth ? "1" : "0");
+    const color = container.querySelector("#text-color").value.replace("#", "");
+    url += "&color=" + color;
+    if (container.querySelector("#text-bg").checked) {
+      container.querySelector(".only-show-if-bg-color").style.visibility = "visible";
+      const background = container.querySelector("#text-background").value.replace("#", "");
+      url += "&background=" + background;
+      const corners = container.querySelector("#text-corners").value;
+      url += "&corners=" + corners;
+    } else {
+      container.querySelector(".only-show-if-bg-color").style.visibility = "hidden";
     }
+    const font = container.querySelector("#text-font").value;
+    url += "&font=" + font;
+    const text = container.querySelector("#text-text").value;
+    url += "&text=" + encodeURIComponent(text);
+    container.querySelector(".output").value = url;
+    container.querySelector("iframe").setAttribute("src", url);
   }
 
   // WIDGETS THEMSELVES:
