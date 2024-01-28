39f792287fe7ecf45244ec9a2744a7ca91db8efa
petracoding <petracoding@outlook.com>
2024-01-28 15:33:37 +0100

    Fix problems with Notion widget

--- js/subpages/notion-widgets.js
+++ js/subpages/notion-widgets.js
@@ -16,18 +16,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
       url += "&centered=" + (centered ? "1" : "0");
       const size = container.querySelector("#text-size").value;
       url += "&size=" + size;
+      const fullWidth = container.querySelector("#text-fullWidth").checked;
+      url += "&fullWidth=" + (fullWidth ? "1" : "0");
       const color = container.querySelector("#text-color").value.replace("#", "");
       url += "&color=" + color;
       if (container.querySelector("#text-bg").checked) {
+        container.querySelector(".only-show-if-bg-color").style.visibility = "visible";
         const background = container.querySelector("#text-background").value.replace("#", "");
         url += "&background=" + background;
         const corners = container.querySelector("#text-corners").value;
         url += "&corners=" + corners;
+      } else {
+        container.querySelector(".only-show-if-bg-color").style.visibility = "hidden";
       }
       const font = container.querySelector("#text-font").value;
       url += "&font=" + font;
       const text = container.querySelector("#text-text").value;
-      url += "&text=" + text;
+      url += "&text=" + encodeURIComponent(text);
       container.querySelector(".output").value = url;
       container.querySelector("iframe").setAttribute("src", url);
     }
@@ -37,28 +42,34 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   // TEXT
   if (document.querySelector("#notion-widget-text")) {
-    const mode = findGetParameter("mode");
-    const text = findGetParameter("text");
-    const size = findGetParameter("size");
+    const background = findGetParameter("background");
+    const centered = findGetParameter("centered");
     const color = findGetParameter("color");
     const corners = findGetParameter("corners");
     const font = findGetParameter("font");
-    const background = findGetParameter("background");
+    const fullWidth = findGetParameter("fullWidth");
+    const mode = findGetParameter("mode");
+    const size = findGetParameter("size");
+    const text = findGetParameter("text");
 
     if (mode == "dark") document.body.classList.add("dark-mode");
 
     const textEl = this.querySelector("#text");
 
-    if (findGetParameter("centered") == "1") {
-      textEl.classList.add("centered");
+    if (centered == "1") {
+      textEl.closest("div:not(#text)").classList.add("centered");
     }
     if (size) textEl.style.fontSize = size + "px";
     if (font) textEl.style.fontFamily = font;
     if (color) textEl.style.color = "#" + color;
     if (corners) textEl.style.borderRadius = corners + "px";
-    if (background) textEl.style.backgroundColor = "#" + background;
+    if (fullWidth == "1") textEl.style.width = "100%";
+    if (background) {
+      textEl.style.backgroundColor = "#" + background;
+      textEl.style.padding = "0 0.5em";
+    }
 
-    textEl.innerHTML = text;
+    textEl.innerHTML = decodeURIComponent(text);
   }
 });
 
