865c0f7536cf97b5318a1d3b63cd5a76da4750ac
petracoding <petracoding@outlook.com>
2024-11-16 14:30:14 +0100

    get out (leave) right now

--- js/main.js
+++ js/main.js
@@ -35,6 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Localhost only:
   localhostHelper();
+
+  // SNOW
+  //   initSnow();
 });
 
 function prepareScrollToTop() {
@@ -147,7 +150,7 @@ function updateSidebarTableOfContentsPosition(el, allSections) {
 }
 
 function initTooltips() {
-  tippy("[title]", {
+  tippy("*:not(iframe)[title]", {
     arrow: true,
     placement: "top", // top, right, bottom, left
     delay: 5, //ms
@@ -249,5 +252,70 @@ function doActiveLinks() {
     codingHelpMenu.classList.add("active");
     document.querySelector("#menu-more").removeAttribute("open");
     document.querySelector("#menu-more .active").classList.remove("active");
+  }
+}
+
+function initSnow() {
+  /* https://embed.im/snow */
+  var embedimSnow = document.getElementById("embedim--snow");
+  if (!embedimSnow) {
+    function embRand(a, b) {
+      return Math.floor(Math.random() * (b - a + 1)) + a;
+    }
+    var embCSS = ".embedim-snow{position: absolute;width: 9px;height: 9px;background: white;border-radius: 50%;margin-top:-10px}";
+    var embHTML = "";
+    var i = 1;
+    for (i = 1; i < 200; i++) {
+      embHTML += '<i class="embedim-snow"></i>';
+      var rndX = embRand(0, 1000000) * 0.0001,
+        rndO = embRand(-100000, 100000) * 0.0001,
+        rndT = (embRand(3, 8) * 10).toFixed(2),
+        rndS = (embRand(0, 10000) * 0.0001).toFixed(2);
+      embCSS +=
+        ".embedim-snow:nth-child(" +
+        i +
+        "){" +
+        "opacity:" +
+        (embRand(1, 10000) * 0.0001).toFixed(2) +
+        ";" +
+        "transform:translate(" +
+        rndX.toFixed(2) +
+        "vw,-10px) scale(" +
+        rndS +
+        ");" +
+        "animation:fall-" +
+        i +
+        " " +
+        embRand(10, 30) +
+        "s -" +
+        embRand(0, 30) +
+        "s linear infinite" +
+        "}" +
+        "@keyframes fall-" +
+        i +
+        "{" +
+        rndT +
+        "%{" +
+        "transform:translate(" +
+        (rndX + rndO).toFixed(2) +
+        "vw," +
+        rndT +
+        "vh) scale(" +
+        rndS +
+        ")" +
+        "}" +
+        "to{" +
+        "transform:translate(" +
+        (rndX + rndO / 2).toFixed(2) +
+        "vw, 105vh) scale(" +
+        rndS +
+        ")" +
+        "}" +
+        "}";
+    }
+    embedimSnow = document.createElement("div");
+    embedimSnow.id = "embedim--snow";
+    embedimSnow.innerHTML = "<style>#embedim--snow{position:fixed;left:0;top:0;bottom:0;width:100vw;height:100vh;overflow:hidden;z-index:9999999;pointer-events:none}" + embCSS + "</style>" + embHTML;
+    document.body.appendChild(embedimSnow);
   }
 }
