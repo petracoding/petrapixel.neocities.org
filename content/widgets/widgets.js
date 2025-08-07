7879ab99e29bd7bba9e0aac30a8454349dfe90f7
petracoding <petracoding@outlook.com>
2025-08-07 13:12:36 +0200

    one way or another

--- content/widgets/widgets.js
+++ content/widgets/widgets.js
@@ -1,26 +1,62 @@
+const knownParams = ["fontSize", "center", "font", "marquee", "linkColor", "username", "hideUsername", "hideEmoji", "hideUsernameAndTime", "swapPositions", "delimiter", "underline"];
+
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("%c Widget provided by petrapixel !!!", "font-size: 14pt;");
-  console.log("%c Get yours: https://petrapixel.neocities.org/coding/widgets", "font-size: 12pt;");
+  console.log("%c Widget provided by petrapixel", "font-size: 16pt;");
+  console.log("%c Get yours: https://petrapixel.neocities.org/coding/widgets", "font-size: 14pt;");
 
   const params = getParameters();
-  if (params["color"]) document.body.style.color = params["color"] == "black" || params["color"] == "white" ? params["color"] : "#" + params["color"];
-  if (params["fontSize"]) document.body.style.fontSize = params["fontSize"];
-  if (params["center"]) document.body.style.textAlign = params["center"] == "1" ? "center" : "left";
-  if (params["font"]) document.body.style.fontFamily = '"' + params["font"].replaceAll("%20", " ") + '"';
-
-  if (params["center"]) {
-    if (params["center"] == "0") {
-      document.body.querySelector("main").style.margin = "0";
-    }
-  }
+  doCSS(params);
 
   initStatuscafe(params);
   initLastFm(params);
 });
 
 function getParameters() {
-  let prmstr = window.location.search.substr(1);
-  return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+  let queryString = window.location.search.substr(1).replaceAll(";", "&");
+  let hashString = window.location.hash || ""; // in case of color codes with "#"
+  if (!queryString) return [];
+  return new URLSearchParams(queryString + hashString);
+}
+
+function doCSS(params) {
+  // Known:
+  if (params.get("center")) {
+    document.body.style.textAlign = params.get("center") == "1" ? "center" : "left";
+
+    if (params.get("center") == "0") {
+      document.body.querySelector("main").style.margin = "0";
+    }
+  }
+
+  // Backwards Compability (now unknown):
+  if (params.get("font")) document.body.style.fontFamily = '"' + params.get("font").replaceAll("%20", " ") + '"';
+  if (params.get("fontSize")) document.body.style.fontSize = params.get("fontSize");
+
+  // Use unknown parameters as CSS:
+  const unknownParams = [];
+  params.forEach((v, k) => {
+    if (!knownParams.includes(k)) {
+      if (k.includes(":") && !v) {
+        // fix e.g. font-size:1px to font-size=1px
+        unknownParams.push([k.split(":")[0], k.split(":")[1]]);
+      } else {
+        unknownParams.push([k, v]);
+      }
+    }
+  });
+  let cssCode = document.body.getAttribute("style") + ";";
+  unknownParams.forEach((p) => {
+    if (p[0] == "color" || p[0] == "background-color") {
+      // deal with colors
+      const hasHash = p[1].includes("#");
+      const needsHash = p[1] !== "black" && p[1] !== "white" && !hasHash;
+      cssCode += p[0] + ":" + (needsHash ? "#" : "") + p[1] + ";";
+    } else {
+      cssCode += p[0] + ":" + p[1].replaceAll("%20", " ") + ";";
+    }
+  });
+
+  document.body.setAttribute("style", cssCode);
 }
 
 function transformToAssocArray(prmstr) {
@@ -50,11 +86,11 @@ function turnIntoMarquee() {
 function initStatuscafe(params) {
   if (!document.getElementById("statuscafe-username")) return;
 
-  if (params["marquee"]) {
-    if (params["marquee"] == "1") turnIntoMarquee();
+  if (params.get("marquee")) {
+    if (params.get("marquee") == "1") turnIntoMarquee();
   }
 
-  let username = params["username"];
+  let username = params.get("username");
   if (!username) {
     document.getElementById("statuscafe-content").innerHTML = "Please enter a username.";
   }
@@ -68,15 +104,15 @@ function initStatuscafe(params) {
         return;
       }
       let delimiter = "";
-      if (params["marquee"]) {
-        if (params["marquee"] == "1") {
+      if (params.get("marquee")) {
+        if (params.get("marquee") == "1") {
           delimiter = ": ";
         }
       }
 
       let emoji = r.face;
-      if (params["hideEmoji"]) {
-        if (params["hideEmoji"] == "1") emoji = "";
+      if (params.get("hideEmoji")) {
+        if (params.get("hideEmoji") == "1") emoji = "";
       }
 
       document.getElementById("statuscafe-username").innerHTML =
@@ -84,22 +120,23 @@ function initStatuscafe(params) {
       document.getElementById("statuscafe-content").innerHTML = username == "petra1999" ? "this is an example status text!" : r.content;
 
       // Styling:
-      if (params["linkColor"])
-        document.querySelector("#statuscafe-username a").style.color = params["linkColor"] == "black" || params["linkColor"] == "white" ? params["linkColor"] : "#" + params["linkColor"];
-      if (params["hideUsernameAndTime"]) {
-        if (params["hideUsernameAndTime"] == "1") document.querySelector("#statuscafe-username").style.display = "none";
+      if (params.get("linkColor"))
+        document.querySelector("#statuscafe-username a").style.color =
+          params.get("linkColor") == "black" || params.get("linkColor") == "white" ? params.get("linkColor") : "#" + params.get("linkColor");
+      if (params.get("hideUsernameAndTime")) {
+        if (params.get("hideUsernameAndTime") == "1") document.querySelector("#statuscafe-username").style.display = "none";
       }
-      if (params["hideUsername"]) {
-        if (params["hideUsername"] == "1") document.querySelector("#statuscafe-username a").style.display = "none";
+      if (params.get("hideUsername")) {
+        if (params.get("hideUsername") == "1") document.querySelector("#statuscafe-username a").style.display = "none";
       }
-      if (params["fip"]) {
-        if (params["fip"] == "1") {
+      if (params.get("swapPositions")) {
+        if (params.get("swapPositions") == "1") {
           document.querySelector("#marquee-holder").style.display = "flex";
           document.querySelector("#marquee-holder").style.flexDirection = "column-reverse";
         }
       }
-      // if (params["marquee"]) {
-      //   if (params["marquee"] == "1") {
+      // if (params.get("marquee")) {
+      //   if (params.get("marquee") == "1") {
       //     document.querySelector("main").style.width = "100%";
       //     document.querySelector("marquee").style.display = "flex";
       //     document.querySelector("marquee").style.alignItems = "end";
@@ -117,27 +154,28 @@ function initStatuscafe(params) {
 function initLastFm(params) {
   if (!document.querySelector("#song")) return;
 
-  if (params["marquee"]) {
-    if (params["marquee"] == "1") turnIntoMarquee();
+  if (params.get("marquee")) {
+    if (params.get("marquee") == "1") turnIntoMarquee();
   }
 
   // Styling:
-  if (params["color"]) document.querySelector("#song").style.color = params["color"] == "black" || params["color"] == "white" ? params["color"] : "#" + params["color"];
-  if (params["linkColor"]) document.querySelector("#song").style.color = params["linkColor"] == "black" || params["linkColor"] == "white" ? params["linkColor"] : "#" + params["linkColor"];
-  if (params["underline"]) {
-    if (params["underline"] == "0") document.querySelector("#song").style.textDecoration = "none";
+  if (params.get("color")) document.querySelector("#song").style.color = params.get("color") == "black" || params.get("color") == "white" ? params.get("color") : "#" + params.get("color");
+  if (params.get("linkColor"))
+    document.querySelector("#song").style.color = params.get("linkColor") == "black" || params.get("linkColor") == "white" ? params.get("linkColor") : "#" + params.get("linkColor");
+  if (params.get("underline")) {
+    if (params.get("underline") == "0") document.querySelector("#song").style.textDecoration = "none";
   }
-  if (params["marquee"]) {
-    if (params["marquee"] == "1") document.querySelector("#song").style.whiteSpace = "nowrap";
+  if (params.get("marquee")) {
+    if (params.get("marquee") == "1") document.querySelector("#song").style.whiteSpace = "nowrap";
   }
 
-  let username = params["username"];
+  let username = params.get("username");
   if (!username) {
     document.getElementById("statuscafe-content").innerHTML = "Please enter a username.";
   }
   username = username.trim();
 
-  let delimiter = params["delimiter"];
+  let delimiter = params.get("delimiter");
   if (!delimiter) {
     delimiter = "–";
   }
@@ -148,8 +186,8 @@ function initLastFm(params) {
   }
 
   let swapPositions = false;
-  if (params["swapPositions"]) {
-    if (params["swapPositions"] == "1") swapPositions = true;
+  if (params.get("swapPositions")) {
+    if (params.get("swapPositions") == "1") swapPositions = true;
   }
 
   let url = "https://lastfm-last-played.biancarosa.com.br/" + username + "/latest-song";
